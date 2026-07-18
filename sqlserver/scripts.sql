b9867ac952cbd4340d765fd60b619ec782d38ec0
MatiuC <eduardo.castro01@epn.edu.ec>
2026-07-18 05:46:11 -0500

    Creacion de archivos docker y carpetas.

---
+++ sqlserver/scripts.sql
@@ -0,0 +1,10 @@
+-- El Integrante 1 debe llenar este archivo con las instrucciones para crear la BD, tablas y datos con errores
+
+CREATE DATABASE ETL_Origen;
+GO
+
+USE ETL_Origen;
+GO
+
+-- Ejemplo (reemplazar con el diseño real):
+-- CREATE TABLE Ventas ( ... );
